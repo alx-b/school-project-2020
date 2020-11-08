0d98668deb0212e02e477ba9fdab9636ac94e297
alx-b <none>
2020-11-08 12:40:19 +0100

    Restructuring project's folders.

--- statics/posts/post.js
+++
@@ -1,9 +0,0 @@
-    container = document.querySelector(".post-del-container");
-    container.style.display = "none";
-
-    delButton = document.querySelector(".post-delete-button");
-    delButton.addEventListener("click", showHTML);
-
-    function showHTML(){
-        container.style.display = "grid";
-    }
